693b1ad36623527ccf86ef33460a0ed0758694f1
ssoyeeon <ssuu2039@gmail.com>
2025-01-07 21:34:45 +0900

    [feat] 헤헤..

--- GameMath/Assets/Scripts/PlayerController.cs
+++ GameMath/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,19 @@ using UnityEngine;
 
 public class PlayerController : PortalableObject
 {
-    private CameraMove cameraMove;
+    //물체를 잡을 수 있는 레이어는 하지말고, 태그로 변경
+    //물체를 잡고 놓을 때 해야해
+    //물체 잡기, 놓기
+    public Texture2D crosshair;         //크로스헤어 이미지 
+    private float crosshairSize = 20f;  //크로스헤어 사이즈
+
+    private CameraMove cameraMove;      //카메라 이동
+
+    private Collider heldCollider;      //현재 들고있는 물체의 콜라이더
+    private Rigidbody heldrigidbody;    //현재 들고있는 물체의 리지드바디
+    private GameObject heldObject;
+
+    public Camera playerCamera;
 
     protected override void Awake()
     {
@@ -10,6 +22,31 @@ public class PlayerController : PortalableObject
         base.Awake();
         // CameraMove 컴포넌트 가져오기
         cameraMove = GetComponent<CameraMove>();
+    }
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; // 마우스 커서를 화면 중앙에 고정
+        Cursor.visible = false; // 마우스 커서 숨김
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void TryPickUp(GameObject obj)
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit))
+        {
+            if (obj.tag == "Pick" && hit.collider != null && hit.rigidbody != null)
+            {
+                heldObject = obj;
+                heldCollider.enabled = false;           //물체 부딪히지 않음.
+                heldrigidbody.isKinematic = false;      //물체 회전X
+            }
+        }  
     }
 
     public override void Warp()
---
+++ GameMath/Assets/Scripts/PlayerObject.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerObject : MonoBehaviour
+{
+    //물체 잡기 놓기
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
